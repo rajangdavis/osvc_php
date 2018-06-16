e1dd103ec5184a71f3d764deae4fdb4b54d403c4
rajangdavis <rajangdavis@gmail.com>
2018-06-15 18:28:09 -0700

    trying to improve how the connect function works, it's a little bit verbose

--- src/Connect.php
+++ src/Connect.php
@@ -28,50 +28,77 @@ class Connect extends Client
 		return self::_curl_generic($options,"DELETE");	
 	}
 
+	
+
+
 	private static function _curl_generic($options, $method = "GET")
 	{
-		$client_hash = $options['client'];
-		$resource_url = $options['url'];
-
-		$resource_url_final = isset($resource_url) ? rawurlencode($resource_url) : "";
-		$url = $client_hash->config->base_url . $resource_url;
-		$curl = curl_init();
-		$headers = array(
-			"Content-Type: application/json",
-			"Authorization: Basic " . $client_hash->config->login,
-			"Connection: Keep-Alive",
-			"Keep-Alive: timeout=1, max=1000"
-		);
-		if($client_hash->config->suppress_rules) array_push($headers,"OSvC-CREST-Suppress-All : true");
-		curl_setopt($curl, CURLOPT_URL, $url);
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, !$client_hash->config->no_ssl_verify);
-		curl_setopt($curl, CURLOPT_POST, ($method == "POST")); 
-		if (($method == "POST" || "PATCH") && isset($options['json'])) curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($options['json']));
-		if ($method == "PATCH"){
-			array_push($headers,"X-HTTP-Method-Override: PATCH");
-		}else if($method == "DELETE"){
-			curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'DELETE');
-		}
-		curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-		return self::_run_curl($curl, $options);
+		$curl = self::_init_curl(); 
+		return self::_run_curl($options, $curl);
 	}
 
-	private static function _run_curl($curl, $options){
-		
-		$body = json_decode(curl_exec($curl));
-		$info = curl_getinfo($curl);
-		curl_close($curl);
-
-		if(isset($options['debug']) && $options['debug'] === true){
-			$final_results =  array(
-				'body' => $body,
-				'info'=> $info
-			);
-			return $final_results;
-		}else{
-			return $body;
+		private static function _init_curl($options, $method)
+		{
+			$curl = curl_init();
+			$urlSetForCurl = self::_set_url($options,$curl);
+			return self::_config_curl($options,$urlSetForCurl,$method);
+					
 		}
-	}
+
+			private static function _set_url($options,$curl){
+				$resource_url = $options['url'];
+				$resource_url_final = isset($resource_url) ? rawurlencode($resource_url) : "";
+				$url =  $options['client']->config->base_url . $resource_url;
+				curl_setopt($curl, CURLOPT_URL, $url);
+				curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+				return $curl;
+			}
+
+			private static function _config_curl($options,$curl,$method)
+			{
+				$headers = self::_init_headers($options);
+
+				curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, !$options['client']->config->no_ssl_verify);
+				curl_setopt($urlSetForCurl, CURLOPT_POST, ($method == "POST")); 
+				if (($method == "POST" || "PATCH") && isset($options['json'])) curl_setopt($urlSetForCurl, CURLOPT_POSTFIELDS, json_encode($options['json']));
+				if ($method == "PATCH"){
+					array_push($headers,"X-HTTP-Method-Override: PATCH");
+				}else if($method == "DELETE"){
+					curl_setopt($urlSetForCurl, CURLOPT_CUSTOMREQUEST, 'DELETE');
+				}
+				curl_setopt($urlSetForCurl, CURLOPT_HTTPHEADER, $headers);
+				return $urlSetForCurl;
+			}
+
+				private static function _init_headers($options)
+				{
+					$headers = array(
+						"Content-Type: application/json",
+						"Authorization: Basic " . $options['client']->config->login,
+						"Connection: Keep-Alive",
+						"Keep-Alive: timeout=1, max=1000"
+					);
+
+					if($options['client']->config->suppress_rules) array_push($headers,"OSvC-CREST-Suppress-All : true");
+					return $headers;
+				}
+
+		private static function _run_curl($options,$curl)
+		{
+			
+			$body = json_decode(curl_exec($curl));
+			$info = curl_getinfo($curl);
+			curl_close($curl);
+
+			if(isset($options['debug']) && $options['debug'] === true){
+				$final_results =  array(
+					'body' => $body,
+					'info'=> $info
+				);
+				return $final_results;
+			}else{
+				return $body;
+			}
+		}
 }
 
